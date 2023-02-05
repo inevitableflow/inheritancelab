817d2987e56e63ec7917dfad4f2f25f35e90a5c4
inevitableflow <raquelcastoldi16@gmail.com>
2023-02-05 14:39:17 -0700

    Formatted output

--- inheritancelab/Program.cs
+++ inheritancelab/Program.cs
@@ -21,7 +21,14 @@ namespace inheritancelab
                 emplist.Add(employee);
             }
 
-            Console.WriteLine($"Average weekly pay for all employees: {Avg(emplist)}");
+            Employee highest = Highest(emplist);
+            Employee lowest = Lowest(emplist);
+            double[] percentages = Percentage(emplist);
+
+            Console.WriteLine($"Average weekly pay for all employees: {Avg(emplist):C}");
+            Console.WriteLine($"Highest weekly paid employee is {highest.Name}, earning {highest.GetPay():C} a week");
+            Console.WriteLine($"Lowest weekly paid employee is {lowest.Name}, earning {lowest.GetPay():C} a week");
+            Console.WriteLine($"{percentages[0]:N1}% of employees are salaried. {percentages[1]:N1}% of employees are waged. {percentages[2]:N1}% of employees are part-time. ");
 
         }
 
@@ -103,7 +110,12 @@ namespace inheritancelab
                     partTimeCount += 1;
                 }
             }
-            double[] result = { salariedCount, wageCount, partTimeCount };
+            double[] result =
+                {
+                salariedCount/emplist.Count()* 100,
+                wageCount/emplist.Count()*100, 
+                partTimeCount/emplist.Count()*100
+                };
             return result;
         }
     }
