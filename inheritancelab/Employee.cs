44df486604001973550e22d649b0c88c157ca9dc
inevitableflow <raquelcastoldi16@gmail.com>
2023-02-05 14:15:09 -0700

    Added Highest, Lowest, and Percentage

--- inheritancelab/Employee.cs
+++ inheritancelab/Employee.cs
@@ -35,5 +35,7 @@ namespace inheritancelab
 
             return employee;
         }
+
+
     }
 }
--- inheritancelab/Program.cs
+++ inheritancelab/Program.cs
@@ -21,7 +21,7 @@ namespace inheritancelab
                 emplist.Add(employee);
             }
 
-            Console.WriteLine($"Average weekly pay for all employees: {Avg(emplist)}" );
+            Console.WriteLine($"Average weekly pay for all employees: {Avg(emplist)}");
 
         }
 
@@ -30,11 +30,13 @@ namespace inheritancelab
             Employee e = null;
             if (line[0] >= '0' && line[0] <= '4')
             {
-               e = Salaried.Parse(line);
-            } else if (line[0] >= '5' && line[0] <= '7')
+                e = Salaried.Parse(line);
+            }
+            else if (line[0] >= '5' && line[0] <= '7')
             {
                 e = Wages.Parse(line);
-            } else if (line[0] >= 8 && line[0] <= '9')
+            }
+            else if (line[0] >= 8 && line[0] <= '9')
 
             {
                 e = PartTime.Parse(line);
@@ -47,11 +49,62 @@ namespace inheritancelab
             foreach (Employee emp in emplist)
             {
                 totalPay += emp.GetPay();
-                Console.WriteLine(emp.Name);
-                Console.WriteLine(emp.GetPay());
             }
             return totalPay / emplist.Count;
 
+        }
+
+        static Employee Highest(List<Employee> emplist)
+        {
+            Employee h = emplist.First();
+
+            foreach (Employee emp in emplist)
+            {
+                if (emp.GetPay() > h.GetPay())
+                {
+                    h = emp;
+                }
+            }
+            return h;
+        }
+
+        static Employee Lowest(List<Employee> emplist)
+        {
+            Employee h = emplist.First();
+
+            foreach (Employee emp in emplist)
+            {
+                if (emp.GetPay() < h.GetPay())
+                {
+                    h = emp;
+                }
+            }
+            return h;
+        }
+
+        static double[] Percentage(List<Employee> emplist)
+        {
+            double salariedCount = 0;
+            double wageCount = 0;
+            double partTimeCount = 0;
+
+            foreach (Employee emp in emplist)
+            {
+                if (emp is Salaried)
+                {
+                    salariedCount += 1;
+                }
+                else if (emp is Wages)
+                {
+                    wageCount += 1;
+                }
+                else if (emp is PartTime)
+                {
+                    partTimeCount += 1;
+                }
+            }
+            double[] result = { salariedCount, wageCount, partTimeCount };
+            return result;
         }
     }
 }
