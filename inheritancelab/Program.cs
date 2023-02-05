2374fb5a9e7a2710366bcdcb7e3e42516249240a
inevitableflow <raquelcastoldi16@gmail.com>
2023-02-04 22:37:16 -0700

    filedl a list with objects based on supplied data file

--- inheritancelab/Program.cs
+++ inheritancelab/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,17 @@ namespace inheritancelab
     {
         static void Main(string[] args)
         {
+            string path = @"C:\Users\15875\source\repos\inheritancelab\inheritancelab\res\employees.txt"; // args[0];
+            List<Employee> emplist = new List<Employee>();
+
+            foreach (string line in System.IO.File.ReadLines(path))
+            {
+                Employee employee = Employee.Parse(line);
+
+                emplist.Add(employee);
+            }
+
+            emplist.Sort();
         }
-    }
+    }           
 }
